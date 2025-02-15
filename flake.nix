15d5c6318b6978396bd8e3e74f31cfe95f3d24b9
oscilococcinum <bargaw28@gmail.com>
2025-02-16 00:06:55 +0100

    refactors

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    appimage-file-beta = {
+      url = "https://github.com/zen-browser/desktop/releases/download/1.7.3b/zen-x86_64.AppImage";
+      flake = false;
+    };
+    appimage-file-twilight = {
+      url = "https://github.com/zen-browser/desktop/releases/download/twilight/zen-x86_64.AppImage";
+      flake = false;
+    };
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map (system:
-      {
+    packages = builtins.listToAttrs (map (system: {
         name = system;
         value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
 
-          zen-beta-appimage = pkgs.callPackage (import ./zen-browser) { };
+          zen-beta-appimage = pkgs.callPackage (import ./zen-browser/appimage-default.nix) { src = inputs.appimage-file-beta; pname = "zen"; version = "beta"; };
 
-          zen-twilight-appimage = pkgs.callPackage (import ./zen-browser/twilight.nix) { };
+          zen-twilight-appimage = pkgs.callPackage (import ./zen-browser/appimage-default.nix) { src = inputs.appimage-file-twilight; pname = "zen"; version = "twilight"; };
 
           default = zen-beta-appimage;
         };
-      }
-    )[ "x86_64-linux" ]);
+      })[ "x86_64-linux" ]);
   };
 }
---
+++ zen-browser/appimage-default.nix
@@ -0,0 +1,10 @@
+{
+  appimageTools,
+  src,
+  pname,
+  version
+}:
+
+appimageTools.wrapType2 {
+  inherit src pname version;
+}
